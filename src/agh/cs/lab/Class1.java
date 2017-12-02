79583a12f097a48b6e92e846a7e542941faf0cdb
Szymon Bernal <szymon.bernal@gmail.com>
2017-12-02 16:09:59 +0100

    matching by chapters

---
+++ src/agh/cs/lab/Class1.java
@@ -0,0 +1,39 @@
+package agh.cs.lab;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Class1 {
+   public static void main(String args[]) throws IOException{
+       FileInputStream in = null;
+       FileOutputStream out = null;
+       String text = new String();
+       try (BufferedReader br = new BufferedReader(new FileReader("E:\\konstytucja.txt"))) {
+           String line;
+           while ((line = br.readLine()) != null) {
+              text = text  + "\n" + line;
+
+           }
+       }
+
+       Writer writer = new BufferedWriter(new OutputStreamWriter(
+               new FileOutputStream("filename.txt"), "utf-8"));
+       writer.write(text);
+
+       // Create a Pattern object
+       Pattern r = Pattern.compile("(Rozdział|DZIAŁ)([\\S\\s]*?(?=Rozdział|DZIAŁ|$))");
+
+       // Now create matcher object.
+       Matcher m = r.matcher(text);
+
+
+       while(m.find()) {
+           System.out.println(m.group(0));
+           System.out.println("------");
+       }
+
+   }
+}
