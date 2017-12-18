f10012275ee72ffe92d704a8640266aa8d31d823
Szymon Bernal <szymon.bernal@gmail.com>
2017-12-18 16:43:58 +0100

    updated todo code refactoring

--- src/agh/cs/lab/Class1.java
+++ src/agh/cs/lab/Class1.java
@@ -5,16 +5,16 @@ import java.io.*;
 public class Class1 {
    public static void main(String args[]) throws IOException{
 
-       FileInputStream in = null;
-       FileOutputStream out = null;
-       String text = new String();
-       try (BufferedReader br = new BufferedReader(new FileReader("E:\\konstytucja.txt"))) {
+       StringBuilder text = new StringBuilder();
+       try (BufferedReader br = new BufferedReader(new FileReader("uokik.txt"))) {
            String line;
            while ((line = br.readLine()) != null) {
-              text = text  + "\n" + line;
-
+              text.append(line);
+              text.append("\n");
            }
        }
+
+       //TODO pominięte wyciąć
 
        //TODO Content nie moze być stringiem -> jakaś lista wierszy, uprzednio pozbawiona znaków nowej linii i myślników na końcach?
 
@@ -24,6 +24,9 @@ public class Class1 {
 
        //TODO Pozostałe regexy
 
+       //TODO regularexpression nie ma być statyczne
+
+       //TODO regularexpressions do ENUMA
 
 //       // Create a Pattern object
 //       Pattern r = Pattern.compile("((Rozdział|DZIAŁ)( [XIVL]{1,})([\\S\\s]*?)(?=\\2|$))");
@@ -37,7 +40,7 @@ public class Class1 {
 //           System.out.println("------");
 //       }
 
-       TextPart textPart = new TextPart(TextPartType.Root, new StringBuilder(text));
+       TextPart textPart = new TextPart(TextPartType.Root, text);
        textPart.testowa();
    }
 }
